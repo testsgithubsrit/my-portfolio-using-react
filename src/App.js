436df1ddaa25bff0d821454530c7a596db35680f
Neha <nehagarhwa5@gmail.com>
2025-01-04 11:17:55 +0530

    spcae jj

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Navbar from './Components/navbar/Navbar';
  import Contact from './Components/Contact/Contact';
 import Experience from './Components/Experience/Experience';
 
-
 function App() { 
   return (
     <>
